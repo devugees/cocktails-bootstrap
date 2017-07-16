882368548f8351f9243f487e1498bdb1dffc9e3c
Thomas Kuhnert <tfkuhnert@gmail.com>
2017-07-16 21:11:46 +0200

    index.js welcome template structure, replace images, adjust css

---
+++ js/index.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $(function(){
+    $("#searchContainer").load("search.html"); 
+  });
+  $(function(){
+    $("#sliderContainer").load("slider.html"); 
+  });
+  $(function(){
+    $("#featuresContainer").load("features.html"); 
+  });
+});
