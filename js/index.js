839e69791a7ce34b82507af25a66406b4692f666
Felix Schaumann <felix.schaumann@annalect.com>
2017-07-19 10:27:58 +0200

    Fix index.html

--- js/index.js
+++ js/index.js
@@ -1,41 +1,28 @@
-
 $(document).ready(function() {
-  $(function(){
-    $("#navContainer").load("nav.html");
-  });
-  $(function(){
-    $("#searchContainer").load("search.html");
-  });
-  $(function(){
-    $("#sliderContainer").load("slider.html");
-  });
-  $(function(){
-    //$("#featuresContainer").load("features.html");
-  });
-  $(function(){
+  $("#navContainer").load("nav.html");
+  $("#searchContainer").load("search.html");
+  $("#sliderContainer").load("slider.html");
+  $("#featuresContainer").load("features.html");
   $("#popularContainer").load("popular.html");
-  });
-  $(function(){
-    $("#contactsContainer").load("contacts.html");
-  });
+  $("#contactsContainer").load("contacts.html");
 
-  $(document).on('click','a.page-scroll', function(event) {
+  $(document).on('click', 'a.page-scroll', function(event) {
 
-  var $ele = $(this);
-  $('html, body').stop().animate({
-    scrollTop: ($($ele.attr('href')).offset().top - 60)
-  }, 600);
-  event.preventDefault();
+    var $ele = $(this);
+    $('html, body').stop().animate({
+      scrollTop: ($($ele.attr('href')).offset().top - 60)
+    }, 600);
+    event.preventDefault();
+  });
 });
-});
-  jQuery(document).ready(function() {
+jQuery(document).ready(function() {
   jQuery(window).scroll(function() {
-    var navbar= jQuery("#navContainer").offset().top;
+    var navbar = jQuery("#navContainer").offset().top;
     var scrollpos = jQuery(window).scrollTop();
-    if (scrollpos >= navbar){
-      jQuery("nav").addClass("navbar-fixed-top")
+    if (scrollpos >= navbar) {
+      jQuery("nav").addClass("navbar-fixed-top");
     } else {
-      jQuery("nav").removeClass("navbar-fixed-top")
+      jQuery("nav").removeClass("navbar-fixed-top");
     }
   });
 });
