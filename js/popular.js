7088891381b0061eb6450315266e2fcfba4b35d4
Felix Schaumann <felix.schaumann@annalect.com>
2017-07-20 13:59:18 +0200

    fix popular search bar value

--- js/popular.js
+++ js/popular.js
@@ -1,35 +1,36 @@
 $(document).ready(function() {
-$("#popularContainer").load("popular.html", function(){
+  $("#popularContainer").load("popular.html", function() {
 
-var ratingIn = $('.rating-tooltip');
+    var ratingIn = $('.rating-tooltip');
 
-   ratingIn.each(function(i){
-     $('.rating-tooltip').eq(i).rating('rate', Cookies.get('rating'+ i));
-   });
-
-  $('#popularContainer').on('click',"a", function() {
-     var titel_cocktail = $(this).find('h3').text();
+    ratingIn.each(function(i) {
+      $('.rating-tooltip').eq(i).rating('rate', Cookies.get('rating' + i));
     });
 
-  $('input.check').on('change', function () {
-    var rating =  $(this).val() ;
-    var index = $(this).data('rating-index');
+    $('#popularContainer').on('click', "a", function() {
+      var titel_cocktail = $(this).find('h3').text();
+      $('#searchBar').val(titel_cocktail);
+    });
 
-    Cookies.set('rating' + index, rating);
+    $('input.check').on('change', function() {
+      var rating = $(this).val();
+      var index = $(this).data('rating-index');
+
+      Cookies.set('rating' + index, rating);
+    });
+
+
+    $('.rating-tooltip').rating({
+      extendSymbol: function(rate) {
+        $(this).tooltip({
+          container: 'body',
+          placement: 'bottom',
+          title: 'Rate ' + rate
+        });
+      }
+    });
+
+
   });
-
-
-  $('.rating-tooltip').rating({
-         extendSymbol: function (rate) {
-           $(this).tooltip({
-             container: 'body',
-             placement: 'bottom',
-             title: 'Rate ' + rate
-           });
-         }
-       });
-
-
-});
 
 });
