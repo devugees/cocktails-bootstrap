43a81e505b4ff200bd68d87c29bf496171e2b597
Thomas Kuhnert <tfkuhnert@gmail.com>
2017-07-23 21:28:43 +0200

    switch to sass syntax and watch files per gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,26 @@
 var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
+var sass        = require('gulp-sass');
 
-// Static Server + watching html files
-gulp.task('serve', function() {
+// Static Server + watching sass/html files
+gulp.task('serve', ['sass'], function() {
 
     browserSync.init({
         server: "./"
     });
 
-    gulp.watch("./*.html").on('change', browserSync.reload);
+    gulp.watch("sass/*.sass", ['sass']);
+    gulp.watch("*.html").on('change', browserSync.reload);
 });
 
-
-gulp.task('default', ['serve']);
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+    return gulp.src("css/*.sass")
+        .pipe(sass())
+        .pipe(gulp.dest("css"))
+        .pipe(browserSync.stream());
+});
+gulp.task('sass:watch', function () {
+  gulp.watch('./css/**/*.sass', ['sass']);
+});
+gulp.task('default', ['serve', 'sass:watch']);
