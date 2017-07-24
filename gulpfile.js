ad3d122c4f86b561c884174ec8114b0047953b27
Thomas Kuhnert <tfkuhnert@gmail.com>
2017-07-24 15:00:13 +0200

    update gulp for error handling, minify and autoprefix

--- gulpfile.js
+++ gulpfile.js
@@ -2,26 +2,33 @@ var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
 var sass        = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
+var cleanCSS = require('gulp-clean-css');
+var autoprefixer = require('gulp-autoprefixer');
 
 // Static Server + watching sass/html files
 gulp.task('serve', ['sass'], function() {
 
-    browserSync.init({
-        server: "./"
-    });
+  browserSync.init({
+    server: "./"
+  });
 
-    gulp.watch("sass/*.sass", ['sass']);
-    gulp.watch("*.html").on('change', browserSync.reload);
+  gulp.watch("sass/*.sass", ['sass']);
+  gulp.watch("*.html").on('change', browserSync.reload);
 });
 
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function() {
-    return gulp.src("css/*.sass")
-        .pipe(sourcemaps.init())
-        .pipe(sass())
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest("css"))
-        .pipe(browserSync.stream());
+  return gulp.src("css/*.sass")
+    .pipe(sourcemaps.init())
+    .pipe(sass()).on('error', function(err) {
+      console.error(err.message);
+      browserSync.notify(err.message, 3000); // Display error in the browser
+      this.emit('end'); // Prevent gulp from catching the error and exiting the watch process
+    })
+    .pipe(cleanCSS())
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest("css"))
+    .pipe(browserSync.stream());
 });
 gulp.task('sass:watch', function () {
   gulp.watch('./css/**/*.sass', ['sass']);
